f0094e8ce8262f7e15b0c05c1c24dd56166f9e72
marcio <marcioadrianobraz@gmail.com>
2022-01-12 20:04:58 -0300

    teste 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de temperatura</title>
+</head>
+<body>
+    <form action='temp.php' method ='POST'>
+        <input name="temp" type="number" placeholder = 'Farenheit'>
+        <button type="submit">Converter</button>
+    </form>
+</body>
+</html>
---
+++ temp.php
@@ -0,0 +1,5 @@
+<?php 
+  $F = $_POST['temp'];
+  $C = ($F-32)*5/9;  
+  echo "Em Celsius:". $C;
+ ?>
